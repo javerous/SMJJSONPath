30844becffc13bfe5179eae35f57904557e3dc62
Julien-Pierre Avérous <git@sourcemac.com>
2020-05-02 23:54:35 +0200

    - Bump year.

--- SMJJSONPathTests/Jayway/SMJBaseFunctionTest.h
+++ SMJJSONPathTests/Jayway/SMJBaseFunctionTest.h
@@ -1,7 +1,7 @@
 /*
  * SMJBaseFunctionTest.h
  *
- * Copyright 2019 Avérous Julien-Pierre
+ * Copyright 2020 Avérous Julien-Pierre
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
